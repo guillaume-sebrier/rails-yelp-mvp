b7b29b56388f50f3bde6ac00100a27e352dd8f5d
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-11 15:18:23 +0100

    yelp me

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Generating seeds"
+
+5.times do
+  Restaurant.create([name: Faker::Restaurant.name, category: Restaurant::CATEGORIES.sample, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number])
+end
